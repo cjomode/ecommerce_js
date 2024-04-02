42ebe89aeb3494a77aaac41eca9ee5e8f903f764
Juliana de Oliveira <oliveiramjuliana22@gmail.com>
2024-04-02 11:07:56 -0300

    update changes

--- js/index.js
+++ js/index.js
@@ -10,6 +10,20 @@
 //     }
 // }
 
+// dark mode
+
+
+// <button onclick="myFunction()">Toggle dark mode</button>
+
+// <script>
+// 
+function mudarTema() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+}
+    
+
 var imagem1 = true;
 
 function trocaImagem() {
@@ -38,5 +52,31 @@ function trocaImagem2() {
     imagem2 = !imagem2;
 }
 
+var imagem3 = true;
+
+function trocaImagem3() {
+    var image = document.getElementById('like3');
+
+    if (imagem3) {
+        image.src = "images/heart_2107952.png";
+    } else {
+        image.src = "images/heart_2107774.png";
+    }
+
+    imagem3 = !imagem3;
+}
 
 
+var imagem4 = true;
+
+function trocaImagem4() {
+    var image = document.getElementById('like4');
+
+    if (imagem4) {
+        image.src = "images/heart_2107952.png";
+    } else {
+        image.src = "images/heart_2107774.png";
+    }
+
+    imagem4 = !imagem4;
+}
